e9336cb4f2a276fd02413a37932e451d91da5b4f
ainertec <57117318+Ainertec@users.noreply.github.com>
2020-11-07 11:03:47 -0300

    new update

--- dist/src/services/openShop.js
+++ dist/src/services/openShop.js
@@ -1,16 +1,41 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Shop = void 0;
+var fs_1 = __importDefault(require("fs"));
+var path_1 = __importDefault(require("path"));
 var Shop = /** @class */ (function () {
     function Shop() {
     }
     Shop.setOpen = function (open) {
         this.open = open;
+        try {
+            fs_1.default.writeFile(path_1.default.resolve(__dirname, 'config.txt'), "" + open, { flag: 'w' }, function (err) {
+                if (err)
+                    console.log(err);
+            });
+        }
+        catch (error) { }
     };
     Shop.getOpen = function () {
+        if (this.open == null) {
+            try {
+                fs_1.default.readFile(path_1.default.resolve(__dirname, 'config.txt'), 'utf-8', function (err, data) {
+                    if (err)
+                        console.log(err);
+                    else {
+                        data = JSON.parse(data);
+                        this.open = Boolean(data == 'true');
+                    }
+                });
+            }
+            catch (error) { }
+        }
         return this.open;
     };
-    Shop.open = false;
+    Shop.open = null;
     return Shop;
 }());
 exports.Shop = Shop;
--- src/services/openShop.ts
+++ src/services/openShop.ts
@@ -1,11 +1,30 @@
+import fs from 'fs';
+import path from 'path';
+
 export class Shop {
-    private static open: Boolean = false;
+    private static open = null;
 
     public static setOpen(open: Boolean): void {
         this.open = open;
+        try {
+            fs.writeFile(path.resolve(__dirname, 'config.txt'), `${open}`, { flag: 'w' }, function (err) {
+                if (err) console.log(err);
+            });
+        } catch (error) { }
     }
 
     public static getOpen() {
+        if (this.open == null) {
+            try {
+                fs.readFile(path.resolve(__dirname, 'config.txt'), 'utf-8', function (err, data) {
+                    if (err) console.log(err);
+                    else {
+                        data = JSON.parse(data)
+                        this.open = Boolean(data == 'true');
+                    }
+                });
+            } catch (error) { }
+        }
         return this.open;
     }
 
