ce15d16af5ec1be84ab0330b9e1f343f5c440a02
ainertec <57117318+Ainertec@users.noreply.github.com>
2020-11-07 14:32:17 -0300

    new update

--- dist/src/services/openShop.js
+++ dist/src/services/openShop.js
@@ -1,37 +1,17 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Shop = void 0;
-var fs_1 = __importDefault(require("fs"));
-var path_1 = __importDefault(require("path"));
+var LocalStorage = require('node-localstorage').LocalStorage, localStorage = new LocalStorage('./scratch');
 var Shop = /** @class */ (function () {
     function Shop() {
     }
     Shop.setOpen = function (open) {
         this.open = open;
-        try {
-            fs_1.default.writeFile(path_1.default.resolve(__dirname, 'config.txt'), "" + open, { flag: 'w' }, function (err) {
-                if (err)
-                    console.log(err);
-            });
-        }
-        catch (error) { }
+        localStorage.setItem('open', "" + open);
     };
     Shop.getOpen = function () {
         if (this.open == null) {
-            try {
-                fs_1.default.readFile(path_1.default.resolve(__dirname, 'config.txt'), 'utf-8', function (err, data) {
-                    if (err)
-                        console.log(err);
-                    else {
-                        data = JSON.parse(data);
-                        this.open = Boolean(data == 'true');
-                    }
-                });
-            }
-            catch (error) { }
+            this.open = Boolean(localStorage.getItem('open') == 'true');
         }
         return this.open;
     };
--- src/services/openShop.ts
+++ src/services/openShop.ts
@@ -1,29 +1,17 @@
-import fs from 'fs';
-import path from 'path';
+var LocalStorage = require('node-localstorage').LocalStorage,
+    localStorage = new LocalStorage('./scratch');
 
 export class Shop {
     private static open = null;
 
     public static setOpen(open: Boolean): void {
         this.open = open;
-        try {
-            fs.writeFile(path.resolve(__dirname, 'config.txt'), `${open}`, { flag: 'w' }, function (err) {
-                if (err) console.log(err);
-            });
-        } catch (error) { }
+        localStorage.setItem('open', `${open}`);
     }
 
     public static getOpen() {
         if (this.open == null) {
-            try {
-                fs.readFile(path.resolve(__dirname, 'config.txt'), 'utf-8', function (err, data) {
-                    if (err) console.log(err);
-                    else {
-                        data = JSON.parse(data)
-                        this.open = Boolean(data == 'true');
-                    }
-                });
-            } catch (error) { }
+            this.open = Boolean(localStorage.getItem('open') == 'true')
         }
         return this.open;
     }
